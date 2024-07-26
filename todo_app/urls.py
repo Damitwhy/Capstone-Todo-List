b339c550fcb62718c1dd2bab16d18ff2ba5ade8f
Kenneth Cox <damitwhy@gmail.com>
2024-07-26 12:30:30 +0000

    created DetailView in views, added task.html

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.contrib import admin
-from .views import TaskList
+from .views import TaskList, TaskDetail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Task
 
 # Create your views here.
@@ -10,4 +12,8 @@ class TaskList(ListView):
     context_object_name = 'tasks' 
 
 
+class TaskDetail(DetailView):
+    model = Task
+    template_name = 'todo_app/task.html'
+    context_object_name = 'task'
 
