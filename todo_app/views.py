21003cbc3cb082112d7a43d9a57548b403d8db14
Kenneth Cox <damitwhy@gmail.com>
2024-07-26 20:31:30 +0000

    bug fix with logout feature

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,16 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
-RegisterView = None
+from django.views import View
+from django.shortcuts import redirect
+from django.db import transaction
 
 from .models import Task
+from .forms import PositionForm
 
 # Create your views here.
 
@@ -22,6 +27,16 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('tasks')
 
+
+class MyLogoutView(LogoutView):
+    template_name = 'todo_app/logout.html'
+
+    fields = '__all__'
+    
+    next_page = reverse_lazy('login')
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 
 class TaskList(ListView):
