21d1a6e2f02d93d5855857bbb33e66e67b00d77c
Kenneth Cox <damitwhy@gmail.com>
2024-07-26 15:02:12 +0000

    Made login, logout, register html, added paths and views

--- todo_app/views.py
+++ todo_app/views.py
@@ -2,11 +2,37 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
 
 from .models import Task
 
 # Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'todo_app/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+class CustomLogoutView(LoginView):
+    template_name = 'todo_app/logout.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+class RegisterPage(CreateView):
+    template_name = 'todo_app/register.html'
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 class TaskList(ListView):
     model = Task
