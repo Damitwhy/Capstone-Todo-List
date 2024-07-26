784cc65696ce558a468db578ea58dd82de43c144
Kenneth Cox <damitwhy@gmail.com>
2024-07-26 13:56:32 +0000

    completed full CRUD functionality, create, read, update, delete

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Task
 
 # Create your views here.
@@ -17,3 +19,21 @@ class TaskDetail(DetailView):
     template_name = 'todo_app/task.html'
     context_object_name = 'task'
 
+
+class TaskCreate(CreateView):
+    model = Task
+    template_name = 'todo_app/task_form.html'
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    template_name = 'todo_app/task_form.html'
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    template_name = 'todo_app/task_confirm_delete.html'
+    success_url = reverse_lazy('tasks')
